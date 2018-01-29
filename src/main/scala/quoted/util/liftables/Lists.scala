4db1b71461514d4f21365341ffc858c1d13d5cdf
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-29 09:01:16 -0500

    Remove explicit types splices

--- src/main/scala/quoted/util/liftables/Lists.scala
+++ src/main/scala/quoted/util/liftables/Lists.scala
@@ -4,17 +4,17 @@ package util.liftables
 import scala.quoted.Liftable._
 
 object Lists {
-  implicit def ListIsLiftable[T: Liftable](implicit t: Type[T]): Liftable[List[T]] = {
-    case x :: xs  => '{ (~xs.toExpr).::[~t](~x.toExpr) }
+  implicit def ListIsLiftable[T : Liftable](implicit t: Type[T]): Liftable[List[T]] = {
+    case x :: xs  => '{ ~x.toExpr :: ~xs.toExpr }
     case Nil => '{ Nil: List[~t] }
   }
 
-  implicit class LiftedOps[T: Liftable](list: Expr[List[T]]) {
-    def foldLeft[U](acc: Expr[U])(f: Expr[(U, T) => U])(implicit u: Type[U]): Expr[U] =
-      '{ (~list).foldLeft[~u](~acc)(~f) }
+  implicit class LiftedOps[T : Liftable](list: Expr[List[T]]) {
+    def foldLeft[U : Type](acc: Expr[U])(f: Expr[(U, T) => U]): Expr[U] =
+      '{ (~list).foldLeft(~acc)(~f) }
   }
 
-  implicit class UnrolledOps[T: Liftable](list: List[T]) {
+  implicit class UnrolledOps[T : Liftable](list: List[T]) {
     def unrolledFoldLeft[U](acc: Expr[U])(f: (Expr[U], T) => Expr[U]): Expr[U] = list match {
       case x :: xs => xs.unrolledFoldLeft(f(acc, x))(f)
       case Nil => acc
